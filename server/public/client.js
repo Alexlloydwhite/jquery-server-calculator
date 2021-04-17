5e51ffdbd5be9308f4f19b9fa297416be5a121b6
Alex White <alexlloydwhite@gmail.com>
2021-04-17 10:27:15 -0500

    adding more comments..

--- server/public/client.js
+++ server/public/client.js
@@ -20,14 +20,17 @@ function catchOperator() {
 
 // gets calculationHistory array from server to use on the DOM
 function getAnswer(){
+    // Ajax sends a get request to server for information on /calculate
     $.ajax({
         method: 'GET',
         url: '/calculate',
     })
+        // after getting request, show information on the DOM
         .then(function(response){
             console.log('response from server:', response)
             render(response);
         })
+        // if request fails, display alert on DOM
         .catch(function (error){
             console.log('error from server:', error);
             alert('sorry, could not get answer. Try again L8R!');
@@ -54,21 +57,23 @@ function calculate(){
         operator: operator,
     }
 
-    // send object to the server
+    // Ajax sends object to the server, posts to /calculate
     $.ajax({
         method: 'POST',
         url: '/calculate',
         data: calculateInputs,
     })
+        // After posting information to server, console log on client side to confirm
         .then(function(response){
             console.log('Calculating..', calculateInputs);
         })
+        // if post fails, alert user.
         .catch(function( error ) {
             console.log('error from server:', error);
             alert('Sorry, could not complete task. Try again L8R!');
         })
     
-    // display
+    // sends a get request to server for calculation results
     getAnswer();
 }
 
