af00f93a145a7f82850c91cd9a3cc7c2af6abb3b
Alex White <alexlloydwhite@gmail.com>
2021-04-23 08:44:54 -0500

    changing port to comply with Heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use( express.static( 'server/public' ) );
 
 // tell server to listen on port 5000
-const PORT = 5000;
+const PORT = process.env.port || 5000;
 app.listen( PORT, () => {
     console.log(`listening on port ${PORT}...`);
 })
